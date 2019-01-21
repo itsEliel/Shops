1c46fcfa6ac942698c80e5d31d161bc5c12e189a
itsEliel <43959848+itsEliel@users.noreply.github.com>
2019-01-21 12:07:51 +0200

    Create shops.sql

---
+++ shops.sql
@@ -0,0 +1,40 @@
+USE `essentialmode`;
+
+CREATE TABLE `shops` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`store` varchar(100) NOT NULL,
+	`item` varchar(100) NOT NULL,
+	`price` int(11) NOT NULL,
+
+	PRIMARY KEY (`id`)
+);
+
+INSERT INTO `shops` (store, item, price) VALUES
+	('TwentyFourSeven','bread',30),
+	('TwentyFourSeven','water',15),
+
+	('Liquor','bread',30),
+	('Liquor','water',15),
+
+	('GasStation','bread',30),
+	('GasStation','water',15),
+
+	('Weedshop','bread',30),
+	('Weedshop','water',15),
+
+	('Hotdogstand','bread',30),
+	('Hotdogstand','water',15),
+
+	('Burgerstand','bread',30),
+	('Burgerstand','water',15),
+
+	('Coffeemachine','bread',30),
+	('Coffeemachine','water',15),
+
+	('Sodamachine','bread',30),
+	('Sodamachine','water',15),
+
+	('Snackmachine','bread',30),
+	('Snackmachine','water',15),
+
+;
